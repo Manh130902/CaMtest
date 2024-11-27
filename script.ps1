820f4c374e6da6b39d060c1db1517dd798fd1a00
MinKay <93723285+Manh130902@users.noreply.github.com>
2024-11-27 16:28:51 +0700

    Update and rename script.ps1 to script1.ps1

--- script.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Manh130902/CaMtest/refs/heads/master/testlocal1.ps1' -OutFile 'C:\Temp\testlocal1.ps1'
-powershell -ExecutionPolicy Bypass -File "C:\Temp\testlocal1.ps1"
-Start-Process 'https://www.google.com'
